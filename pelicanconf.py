2698fde18d7d581e3f760af6c3bc5f7540f10b3e
= <=>
2019-09-06 23:36:32 +0100

    changing to the clean blog theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,6 +37,6 @@ DEFAULT_PAGINATION = 10
 #Path for images
 STATIC_PATHS = ['images']
 
-THEME = "/Users/kevinomalley/pelican-themes/clean-blog"
+THEME = "/Users/kevinomalley/pelican-themes/blue-penguin"
 
 
